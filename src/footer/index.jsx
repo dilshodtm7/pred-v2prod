eb4edf767846733b16839e44b8f1f57cebf51ca2
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-06-29 19:18:02 +0500

    Update index.jsx

--- src/footer/index.jsx
+++ src/footer/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Tgicon from "../assets/tg-icon.png";
 import "./index.css";
 
 const index = () => {
@@ -6,7 +7,7 @@ const index = () => {
     <>
       <div className="footer-container">
         <div className="footer">
-                    <a className="as" href="https://t.me/AviaBot_admin">
+          <a className="as" href="https://t.me/AviaBot_admin">
             <button className="btn-footer" >
               <img src={Tgicon} alt="TG" className="tg" />AVIABOT STORE ADMIN
               </button>
