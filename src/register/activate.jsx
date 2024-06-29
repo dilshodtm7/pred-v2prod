5871111108b2a9f11acf7a39cf2ba467a693073f
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-06-29 19:21:25 +0500

    Update activate.jsx

--- src/register/activate.jsx
+++ src/register/activate.jsx
@@ -12,22 +12,22 @@ if (language === "RUS") {
         <hr className="hrr" />
         <span className="spans">Купить</span>
         <div className="d-blocked">
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">15 - Дней  : 1500 Рубль</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">20 - Дней  : 2000 Рубль</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">30 - Дней : 3000 Рубль</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">50 - Дней : 4000 Рубль</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">60 - Дней : 5000 Рубль</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">90 - Дней : 7000 Рубль</button>
         </a>
         </div>
@@ -45,22 +45,22 @@ if (language === "ENG") {
         <hr className="hrr" />
         <span className="spans">BUY</span>
         <div className="d-blocked">
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">15 - day : 15 $</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">20 - day : 20 $</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">30 - day : 30 $</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">50 - day : 40 $</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">60 - day : 50 $</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">90 - day : 70 $</button>
         </a>
         </div>
@@ -77,22 +77,22 @@ if (language === "ENG") {
         <hr className="hrr" />
         <span className="spans">SOTIB OLISH</span>
         <div className="d-blocked">
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">15 - kun uchun : 200 000 so'm</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">20 - kun uchun : 250 000 so'm</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">30 - kun uchun : 350 000 so'm</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">50 - kun uchun : 500 000 so'm</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">60 - kun uchun : 600 000 so'm</button>
         </a>
-        <a href="" className="a">
+        <a href="https://t.me/AviaBot_admin" className="a">
           <button className="btn-buy">90 - kun uchun : 750 000 so'm</button>
         </a>
         </div>
