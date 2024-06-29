3b6a187415cdd255a4367384c37b5995c65d58e7
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-06-29 19:13:32 +0500

    Update index.jsx

--- src/footer/index.jsx
+++ src/footer/index.jsx
@@ -6,7 +6,11 @@ const index = () => {
     <>
       <div className="footer-container">
         <div className="footer">
-          <p>© 2023. All Rights Reserved.</p>
+                    <a className="as" href="https://t.me/AviaBot_admin">
+            <button className="btn-footer" >
+              <img src={Tgicon} alt="TG" className="tg" />AVIABOT STORE ADMIN
+              </button>
+          </a>
         </div>
       </div>
     </>
