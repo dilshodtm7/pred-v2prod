c82046b13bfb055457400a357fbefa27cd96b644
dilshodtm7 <155984333+dilshodtm7@users.noreply.github.com>
2024-06-29 13:10:32 +0500

    Update index.jsx

--- src/buy/index.jsx
+++ src/buy/index.jsx
@@ -15,84 +15,48 @@ import { Link } from "react-router-dom";
 const index = () => {
   return (
     <div className="container-reg">
-      
       <span className="spans">Payment methods</span>
       <div className="d-flexbtn">
-        <Link to="/1win/jet">
-        <button className="btns">
-          <img src={Click} className="jet" alt="" />
-          <span className="spans">Click up</span>
-        </button>
-        </Link>
-
-        <Link to="/1win/aviator">
-        <button className="btns">
-          <img src={Payme} className="jet" alt="" />
-          <span className="spans">Payme
-
-
-          </span>
-
-        </button>
-        </Link>
-
-
         <Link to="/1win/rocket">
         <button className="btns">
           <img src={Perfect} className="jet" alt="" />
           <span className="spans">Perfect money</span>
-
         </button>
         </Link>
         <Link to="/1win/rocket">
         <button className="btns">
           <img src={Mir} className="jet" alt="" />
           <span className="spans">Mir</span>
-
         </button>
         </Link>
         <Link to="/1win/rocket">
         <button className="btns">
           <img src={Mbank} className="jet" alt="" />
           <span className="spans">Mbank</span>
-
         </button>
         </Link>
         <Link to="/1win/rocket">
         <button className="btns">
           <img src={Dcity} className="jet" alt="" />
           <span className="spans">Dcity</span>
-
-        </button>
-        </Link>
-   
-        <Link to="/aviator">
-        <button className="btns">
-          <img src={Payeer} className="jet" alt="" />
-          <span className="spans">Payeer</span>
-
         </button>
         </Link>
         <Link to="/1win/rocket">
         <button className="btns">
           <img src={Binance} className="jet" alt="" />
           <span className="spans">Binance</span>
-
         </button>
-        </Link>
-   
+        </Link>   
         <Link to="/aviator">
         <button className="btns">
           <img src={Usdt} className="jet" alt="" />
           <span className="spans">Usdt trc-20</span>
-
         </button>
         </Link>
         <Link to="/1win/rocket">
         <button className="btns">
           <img src={Bitcoin} className="jet" alt="" />
           <span className="spans">Bitcoin</span>
-
         </button>
         </Link>
       </div>
